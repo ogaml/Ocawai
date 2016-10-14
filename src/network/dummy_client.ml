14093da789bcce0a5c024e0dc18e2f68ad0a14b9
Victor Lanvin <victor.lanvin@ens-cachan.fr>
2016-10-14 21:42:28 +0200

    Cleaned workspace

--- src/network/dummy_client.ml
+++
@@ -1,6 +0,0 @@
-
-let socket = Network_tool.open_connection "127.0.0.1" 1234
-
-let dealer = Dealer.create_dealer socket
-
-let () = dealer#run 
--- src/network/send_recv.mli
+++
@@ -1,9 +0,0 @@
-
-(* SEND *)
-
-val send : Unix.file_descr -> int -> string -> float -> bool
-
-
-(* RECV *)
-
-val recv :  Unix.file_descr -> float -> (int * string) option
