d32fe027cad2c49401a88105d2843c3c498f8005
Théo Winterhalter <isheeft@gmail.com>
2016-10-16 14:50:15 +0200

    Complete CaseInfo

--- src/interface/gui/CaseInfo.ml
+++ src/interface/gui/CaseInfo.ml
@@ -8,6 +8,11 @@ let font = Font.load "resources/fonts/FreeSans.ttf"
 let width  = 220.
 let height = 150.
 
+let color_rgb r g b =
+  let f x = (float_of_int x) /. 255. in
+  let r,g,b = f r, f g, f b in
+  Color.(`RGB RGB.({ r ; g ; b ; a = 1. }))
+
 class case_info = object(self)
 
   method draw : 'a. (module RenderTarget.T with type t = 'a) -> 'a ->
@@ -89,79 +94,101 @@ class case_info = object(self)
           })
       end ;
       (* Case information background *)
-      (* new rectangle_shape
-        ~position:(x,y+.uh_offset+.bh_offset)
-        ~size:(width,height-.uh_offset-.bh_offset)
-        ~fill_color:(Color.rgba 210 230 255 240)
-        ()
-      |> target#draw ; *)
+      let shape =
+        let position, size = OgamlMath.Vector2f.(
+          { x ; y = y+.uh_offset+.bh_offset },
+          { x = width ; y = height-.uh_offset-.bh_offset }
+        ) in
+        Shape.create_rectangle
+          ~position ~size
+          ~color:Color.(
+            `RGB RGB.({ r = 0.82 ; g = 0.9 ; b = 1. ; a = 0.94 })
+          ) ()
+      in
+      Shape.draw (module M) target shape () ;
       (* Unit information *)
-      (* begin match u with
+      begin match u with
       | Some u ->
           drawer (chara ^ "_" ^ u#name) (30.,h-.140.+.bh_offset) ;
           (* Name *)
-          rect_print target u#name font (Color.rgb 33 33 33) (Pix 15) (Pix 2)
-            Left
-            { left=50. ; top=h-.150.+.bh_offset ; width=170. ; height=50. };
+          rect_print (module M) target u#name font (color_rgb 33 33 33) (Pix 15)
+            (Pix 2) Left
+            OgamlMath.FloatRect.({
+              x=50. ; y=h-.150.+.bh_offset ; width=170. ; height=50.
+            });
           (* Player ID *)
-          rect_print target ("#" ^ (string_of_int u#player_id)) font
-            (Color.rgb 77 77 77)
+          rect_print (module M) target ("#" ^ (string_of_int u#player_id)) font
+            (color_rgb 77 77 77)
             (Pix 15) (Pix 2)
             Right
-            { left=50. ; top=h-.150.+.bh_offset ; width=170. ; height=50. } ;
+            OgamlMath.FloatRect.({
+              x=50. ; y=h-.150.+.bh_offset ; width=170. ; height=50.
+            }) ;
           (* Life *)
           drawer "life" (25.,h-.115.+.bh_offset) ;
-          rect_print target
+          rect_print (module M) target
             ((string_of_int u#hp) ^ "/" ^ (string_of_int u#life_max)) font
-            (Color.rgb 77 77 77)
+            (color_rgb 77 77 77)
             (Pix 15) (Pix 2)
             Left
-            { left=37. ; top=h-.125.+.bh_offset ; width=60. ; height=50. };
+            OgamlMath.FloatRect.({
+              x=37. ; y=h-.125.+.bh_offset ; width=60. ; height=50.
+            });
           (* Move Range *)
           drawer "move_range" (110.,h-.115.+.bh_offset) ;
-          rect_print target (string_of_int u#move_range) font
-            (Color.rgb 77 77 77)
+          rect_print (module M) target (string_of_int u#move_range) font
+            (color_rgb 77 77 77)
             (Pix 15) (Pix 2)
             Left
-            { left=125. ; top=h-.125.+.bh_offset ; width=10. ; height=50. };
+            OgamlMath.FloatRect.({
+              x=125. ; y=h-.125.+.bh_offset ; width=10. ; height=50.
+            });
           (* Attack Range *)
           drawer "attack_range" (150.,h-.115.+.bh_offset) ;
-          rect_print target
+          rect_print (module M) target
             ((string_of_int u#min_attack_range)
               ^ "-" ^ (string_of_int u#attack_range))
-            font (Color.rgb 77 77 77) (Pix 15) (Pix 2) Left
-            { left=165. ; top=h-.125.+.bh_offset ; width=30. ; height=50. }
+            font (color_rgb 77 77 77) (Pix 15) (Pix 2) Left
+            OgamlMath.FloatRect.({
+              x=165. ; y=h-.125.+.bh_offset ; width=30. ; height=50.
+            })
       | None -> ()
-      end ; *)
+      end ;
       (* Building information *)
-      (* begin match building with
+      begin match building with
       | Some b ->
           drawer (b_chara ^ "_" ^ b#name) (30.,h-.87.) ;
           (* Name *)
-          rect_print target b#name font (Color.rgb 33 33 33)
+          rect_print (module M) target b#name font (color_rgb 33 33 33)
             (Pix 15) (Pix 2)
             Left
-            { left = 50. ; top = h -. 95. ; width = 170. ; height = 50. } ;
+            OgamlMath.FloatRect.({
+              x = 50. ; y = h -. 95. ; width = 170. ; height = 50.
+            }) ;
           (* Player id *)
           let id = match b#player_id with
             | Some id -> "#" ^ (string_of_int id)
             | None -> "neutral"
           in
-          rect_print target id font
-            (Color.rgb 77 77 77)
+          rect_print (module M) target id font
+            (color_rgb 77 77 77)
             (Pix 15) (Pix 2)
             Right
-            { left = 50. ; top = h -. 95. ; width = 170. ; height = 50. } ;
+            OgamlMath.FloatRect.({
+              x = 50. ; y = h -. 95. ; width = 170. ; height = 50.
+            }) ;
           (* Income *)
           drawer "income" (25.,h-.62.) ;
-          rect_print target
+          rect_print (module M) target
             (string_of_int b#income) font
-            (Color.rgb 77 77 77)
+            (color_rgb 77 77 77)
             (Pix 15) (Pix 2)
             Left
-            { left = 37. ; top = h -. 70. ; width = 60. ; height = 50. }
+            OgamlMath.FloatRect.({
+              x = 37. ; y = h -. 70. ; width = 60. ; height = 50.
+            })
       | None -> ()
-      end ; *)
+      end ;
       (* Tile information *)
       tile_drawer (Tile.get_name tile) (20.,h-.50.) ;
       rect_print (module M) target (Tile.get_name tile) font
